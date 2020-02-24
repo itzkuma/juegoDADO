5ca0673bedbff45a4ddb02b766adb259aa7d8917
itzkuma <60122679+itzkuma@users.noreply.github.com>
2020-02-24 15:42:59 -0500

    Add files via upload

---
+++ euler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace euler_david
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese n");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese x");
+            double x = int.Parse(Console.ReadLine());
+            double serie = 0;
+
+            for (int i= 0; i<=n; i++)
+            {
+                 double ex = Factorial(i);
+                 serie += Math.Pow(x, i)/ex;
+
+
+            }
+            Console.WriteLine(serie);
+        }
+
+        public static double Factorial (double number)
+        {
+            if (number == 0) return 1;
+            if (number == 1) return 1;
+            else return number * Factorial(number - 1);
+        }
+    }
+}
---
+++ seno .cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sin_david
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese n");
+            double n = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese x");
+            double x = double.Parse(Console.ReadLine());
+            double serie = 0;
+            double por = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                double ex = Factorial(2*i+1);
+                serie += Math.Pow(-1, i) / ex;
+                por += (Math.Pow(-1, i) / ex)* (Math.Pow (x, 2*i + 1));
+
+
+
+            }
+            Console.WriteLine(por);
+        }
+
+        public static double Factorial(double number)
+        {
+            if (number == 0) return 1;
+            if (number == 1) return 1;
+            else return number * Factorial(number - 1);
+        }
+    }
+}
